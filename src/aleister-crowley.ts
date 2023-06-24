dbec5e6be1674ec6425abb76e35bde5b81c8fd43
Dark Hole <dark.hole1@yandex.ru>
2023-06-24 14:34:00 +0300

    Add getting access token

--- src/aleister-crowley.ts
+++ src/aleister-crowley.ts
@@ -1,16 +1,37 @@
+import axios from "axios"
+import FormData from 'form-data'
 import { Router } from "express"
 import { Composer } from "grammy"
+import { config } from "./config"
 
 export const aleister = new Composer
 export const treeDiagram = Router()
 
 treeDiagram.get('/oauth', async (req, res) => {
-    if(!req.query.code) {
+    if (!req.query.code) {
         return res.redirect('/')
     }
-    if(!req.query.id) {
+    if (!req.query.id) {
         return res.redirect('/wrong')
     }
+
+    const form = new FormData();
+    form.append('grant_type', 'authorization_code')
+    form.append('client_id', config.shiki.client_id)
+    form.append('client_secret', config.shiki.client_secret)
+    form.append('code', req.query.code)
+    form.append('redirect_uri', 'https://radionoise.darkhole.space/oauth')
+
+    const response = await axios.post(
+        'https://shikimori.me/oauth/token',
+        form,
+        {
+            headers: {
+                ...form.getHeaders(),
+                'User-Agent': 'Radio Noise Bot'
+            }
+        }
+    )
+
     // TODO
-    // axi
 })
