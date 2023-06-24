30d6834274f837e213d8776ca98795b69f9c84b5
Dark Hole <dark.hole1@yandex.ru>
2023-06-24 14:35:44 +0300

    Update config

--- src/models/config.ts
+++ src/models/config.ts
@@ -4,7 +4,8 @@ export const RawConfig = z.object({
     token: z.string(),
     shiki: z.object({
         client_id: z.string(),
-        client_secret: z.string()
+        client_secret: z.string(),
+        name: z.string()
     })
 })
 export type RawConfig = z.infer<typeof RawConfig>
