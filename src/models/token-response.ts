1113b5a53b9a2de3feb20d79a60e163b9457788a
Dark Hole <dark.hole1@yandex.ru>
2023-06-24 14:59:36 +0300

    Add token response model

---
+++ src/models/token-response.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const RawTokenResponse = z.object({
+    access_token: z.string(),
+    token_type: z.string(),
+    expires_in: z.number(),
+    refresh_token: z.string(),
+    scope: z.string(),
+    created_at: z.number()
+})
