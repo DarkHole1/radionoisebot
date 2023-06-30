a721e69e12b787a21590d6cb678787c610e3e64a
Dark Hole <dark.hole1@yandex.ru>
2023-06-30 20:01:50 +0300

    Add logginf

--- src/aleister-crowley.ts
+++ src/aleister-crowley.ts
@@ -86,6 +86,7 @@ aleister.command(
 )
 
 export async function getAuthorizedAPI(id: number) {
+    console.log('Getting api for ' + id)
     let token = tokens[id]
     if (!token) {
         return null
@@ -141,5 +142,6 @@ export async function getAuthorizedAPI(id: number) {
 }
 
 export function loggedIn(id: number) {
+    console.log('Logged in? ' + id)
     return id in tokens
 }
