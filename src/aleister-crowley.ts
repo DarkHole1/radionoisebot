cd182ada89be7342fca54b72430637efec03cf69
Dark Hole <dark.hole1@yandex.ru>
2023-07-28 23:35:35 +0300

    Implement anilist auth

--- src/aleister-crowley.ts
+++ src/aleister-crowley.ts
@@ -11,16 +11,17 @@ treeDiagram.get('/oauth', async (req, res) => {
     if (!req.query.code || typeof req.query.code != 'string') {
         return res.redirect('/')
     }
-    if (!req.query.id || typeof req.query.id != 'string') {
+    if (!req.query.id || typeof req.query.id != 'string' || !req.query.type || typeof req.query.type != 'string') {
         return res.redirect('/wrong.html')
     }
 
-    const { id, code } = req.query
-    console.log('Get code %s %s', id, code)
+    const { id, code, type } = req.query
+    const realType: aogami.SupportedAPI = type == 'anilist' ? 'anilist' : 'shiki'
+    console.log('Get code %s %s %s', id, code, type)
     const token = await aogami.getToken({
-        type: 'shiki',
+        type: realType,
         id: parseInt(id),
-        redirect_uri: getRedirectURI(parseInt(id)),
+        redirect_uri: getRedirectURI(parseInt(id), realType),
         code
     })
     if (token == null) {
@@ -33,15 +34,19 @@ aleister.command(
     'login',
     guard(isPrivateChat, reply('Эта команда работает только в личных сообщениях', { replyToMessage: true })),
     ctx => ctx.reply('Чтобы авторизироваться нажмите на кнопк и разрешите использовать списочек', {
-        reply_markup: new InlineKeyboard().url('Кнопк', aogami.getOAuthURL({
+        reply_markup: new InlineKeyboard().url('Shikimori', aogami.getOAuthURL({
             type: 'shiki',
-            redirect_uri: getRedirectURI(ctx.from!.id)
+            redirect_uri: getRedirectURI(ctx.from!.id, 'shiki')
+        }).toString()).url('Anilist', aogami.getOAuthURL({
+            type: 'anilist',
+            redirect_uri: getRedirectURI(ctx.from!.id, 'anilist')
         }).toString())
     })
 )
 
-function getRedirectURI(id: number) {
+function getRedirectURI(id: number, type: aogami.SupportedAPI) {
     let result = new URL(config.server.oauth)
     result.searchParams.append('id', id.toString())
+    result.searchParams.append('type', type)
     return result
 }
