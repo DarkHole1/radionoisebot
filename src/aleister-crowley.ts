f9930975838fb2d61e875608cb2917641ac64e12
Dark Hole <dark.hole1@yandex.ru>
2023-07-30 13:53:46 +0300

    Add cookie

--- src/aleister-crowley.ts
+++ src/aleister-crowley.ts
@@ -7,21 +7,33 @@ import * as aogami from './aogami-pierce'
 export const aleister = new Composer
 export const treeDiagram = Router()
 
+treeDiagram.get('/oauth/:type', (req, res) => {
+    if (!req.query.id || typeof req.query.id != 'string' || !(['shiki', 'anilist']).includes(req.params.type)) {
+        return res.redirect('/wrong.html')
+    }
+    res.cookie('type', req.params.type)
+    res.cookie('id', req.query.id)
+    return res.redirect(aogami.getOAuthURL({
+        type: req.params.type as aogami.SupportedAPI,
+        redirect_uri: getRedirectURI()
+    }).toString())
+})
+
 treeDiagram.get('/oauth', async (req, res) => {
     if (!req.query.code || typeof req.query.code != 'string') {
         return res.redirect('/')
     }
-    if (!req.query.id || typeof req.query.id != 'string' || !req.query.type || typeof req.query.type != 'string') {
+    if (!req.cookies.id || typeof req.cookies.id != 'string' || !req.query.type || typeof req.query.type != 'string') {
         return res.redirect('/wrong.html')
     }
 
-    const { id, code, type } = req.query
+    const { id, code, type } = req.cookies
     const realType: aogami.SupportedAPI = type == 'anilist' ? 'anilist' : 'shiki'
     console.log('Get code %s %s %s', id, code, type)
     const token = await aogami.getToken({
         type: realType,
         id: parseInt(id),
-        redirect_uri: getRedirectURI(parseInt(id), realType),
+        redirect_uri: getRedirectURI(),
         code
     })
     if (token == null) {
@@ -34,19 +46,16 @@ aleister.command(
     'login',
     guard(isPrivateChat, reply('Эта команда работает только в личных сообщениях', { replyToMessage: true })),
     ctx => ctx.reply('Чтобы авторизироваться нажмите на кнопк и разрешите использовать списочек', {
-        reply_markup: new InlineKeyboard().url('Shikimori', aogami.getOAuthURL({
-            type: 'shiki',
-            redirect_uri: getRedirectURI(ctx.from!.id, 'shiki')
-        }).toString()).url('Anilist', aogami.getOAuthURL({
-            type: 'anilist',
-            redirect_uri: getRedirectURI(ctx.from!.id, 'anilist')
-        }).toString())
+        reply_markup: new InlineKeyboard()
+            .url('Shikimori', getRedirectURI('shiki').toString())
+            .url('Anilist', getRedirectURI('anilist').toString())
     })
 )
 
-function getRedirectURI(id: number, type: aogami.SupportedAPI) {
+function getRedirectURI(base?: aogami.SupportedAPI) {
     let result = new URL(config.server.oauth)
-    result.searchParams.append('id', id.toString())
-    result.searchParams.append('type', type)
+    if (base) {
+        result.pathname += '/' + base
+    }
     return result
 }
