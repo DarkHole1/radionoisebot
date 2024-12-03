3d14abac38d47939d3d7b0df37763f6781e69dab
DarkHole1 <dark.hole1@yandex.ru>
2024-12-03 23:32:15 +0300

    Add manga to tags

--- src/shirai-kuroko.ts
+++ src/shirai-kuroko.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs"
 import { writeFile } from "fs/promises"
-import { Composer, InlineKeyboard } from "grammy"
+import { Composer } from "grammy"
 import { guard, isUserHasId } from "grammy-guard"
 import { makeKeyboard } from './misaka-mikoto'
 import { RawTags } from "./models/tags"
@@ -8,7 +8,7 @@ import { RawTags } from "./models/tags"
 export const shirai = new Composer()
 const isCreatorGuard = guard(isUserHasId(369810644))
 
-const hash2anime = loadFromFile()
+const hash2title = loadFromFile()
 
 shirai.on('msg::hashtag')
     .filter(
@@ -19,13 +19,13 @@ shirai.on('msg::hashtag')
             const hashtag = ctx.entities('hashtag')[0].text
             const reply = ctx.msg.reply_to_message!
             if (!reply.entities) return
-            const anime = reply.entities
+            const title = reply.entities
                 .map(e => ({ ...e, text: reply.text!.substring(e.offset, e.offset + e.length) }))
-                .filter(e => e.type == 'url' && e.text.match(/^https:\/\/shikimori\.(me|one)\/animes\//i))[0].text
+                .filter(e => e.type == 'url' && e.text.match(/^https:\/\/shikimori\.(me|one)\/(anime|manga)s\//i))[0].text
 
-            hash2anime.set(hashtag, anime)
-            await saveToFile(hash2anime)
-            await ctx.reply(`Добавили связь хештега ${hashtag} с аниме ${anime}`)
+            hash2title.set(hashtag, title)
+            await saveToFile(hash2title)
+            await ctx.reply(`Добавили связь хештега ${hashtag} с аниме ${title}`)
         }
     )
 
@@ -33,19 +33,19 @@ shirai.on('msg::hashtag')
 shirai.on(':is_automatic_forward', async ctx => {
     const tags = ctx.entities('hashtag').map(e => e.text)
     console.log('gotcha %s', tags)
-    const animes = tags.flatMap(tag => hash2anime.get(tag) ?? [])
-    for (const anime of animes) {
-        const id = anime2id(anime)
-        if (!id) {
-            return await ctx.reply(anime, {
+    const animes = tags.flatMap(tag => hash2title.get(tag) ?? [])
+    for (const title of animes) {
+        const res = title2id(title)
+        if (!res) {
+            return await ctx.reply(title, {
                 reply_to_message_id: ctx.msg.message_id
             })
         }
-        await ctx.reply(anime, {
-            reply_markup: makeKeyboard('anime', { id }),
+        await ctx.reply(title, {
+            reply_markup: makeKeyboard(res.type, { id: res.id }),
             reply_to_message_id: ctx.msg.message_id,
             link_preview_options: {
-                url: `http://cdn.anime-recommend.ru/previews/${id}.jpg`
+                url: `http://cdn.anime-recommend.ru/previews/${res.id}.jpg`
             }
         })
     }
@@ -68,10 +68,13 @@ async function saveToFile(map: Map<string, string>) {
     await writeFile('data/tags.json', text)
 }
 
-function anime2id(url: string) {
-    const match = url.match(/^https:\/\/shikimori\.(?:me|one)\/animes\/.*?(\d+)/)
+function title2id(url: string) {
+    const match = url.match(/^https:\/\/shikimori\.(?:me|one)\/(anime|manga)s\/.*?(\d+)/)
     if (!match) {
         return null
     }
-    return parseInt(match[1])
+    return {
+        type: match[1],
+        id: parseInt(match[2])
+    }
 }
